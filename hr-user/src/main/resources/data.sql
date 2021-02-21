b24e760a4d747183580f71b45d3ab11720819f6d
Jeferson Almeida <jeferson.almeida@outlook.com>
2021-02-21 18:59:10 -0400

    Database seeding

---
+++ hr-user/src/main/resources/data.sql
@@ -0,0 +1,16 @@
+INSERT INTO tb_user (name, email, password)
+VALUES ('Nina Brown', 'nina@gmail.com', '$2a$12$Xyjfciun/fE2F8/J92qTiuLv4EVvfl5NJLEW/ysQHzO9/wYwUEpu6');
+INSERT INTO tb_user (name, email, password)
+VALUES ('Leia Red', 'leia@gmail.com', '$2a$12$Xyjfciun/fE2F8/J92qTiuLv4EVvfl5NJLEW/ysQHzO9/wYwUEpu6');
+
+INSERT INTO tb_role (role_name)
+VALUES ('ROLE_OPERATOR');
+INSERT INTO tb_role (role_name)
+VALUES ('ROLE_ADMIN');
+
+INSERT INTO tb_user_role (user_id, role_id)
+VALUES (1, 1);
+INSERT INTO tb_user_role (user_id, role_id)
+VALUES (2, 1);
+INSERT INTO tb_user_role (user_id, role_id)
+VALUES (2, 2);
