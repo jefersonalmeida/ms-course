7b8d664a85adc16a8eee0327b01f14174c23205e
Jeferson Almeida <jeferson.almeida@outlook.com>
2021-03-06 09:57:42 -0400

    Login, JWT token

--- hr-oauth/src/main/java/com/jastech/hroauth/services/UserService.java
+++ hr-oauth/src/main/java/com/jastech/hroauth/services/UserService.java
@@ -5,10 +5,13 @@ import com.jastech.hroauth.interfaces.UserFeignClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 
     private static final Logger logger = LoggerFactory.getLogger(UserService.class);
 
@@ -26,6 +29,17 @@ public class UserService {
             throw new IllegalArgumentException("Email not found");
         }
         logger.error("Email found " + email);
+        return user;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userFeignClient.findByEmail(username).getBody();
+        if (user == null) {
+            logger.error("Username not found " + username);
+            throw new UsernameNotFoundException("Username not found");
+        }
+        logger.error("Username found " + username);
         return user;
     }
 }
